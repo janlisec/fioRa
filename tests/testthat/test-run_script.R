a1e319d918c692c2de5356e57c057c90f0a26052
janlisec <jan.lisec@bam.de>
2025-06-23 18:05:16 +0200

    v0.2.4

--- tests/testthat/test-run_script.R
+++ tests/testthat/test-run_script.R
@@ -1,4 +1,5 @@
 testthat::test_that("fioRa `run_script` function works with default parameters", {
+  skip_if(Sys.getenv("CI") == "true", "Skipping test on CI")
   tmp <- fioRa::run_script(Name = "Example_0", SMILES = "CC1=CC(=O)OC2=CC(OS(O)(=O)=O)=CC=C12", Precursor_type = "[M-H]-", CE = 17, Instrument_type = "HCD")
   testthat::expect_length(tmp, 1)
   testthat::expect_length(tmp[[1]], 9)
